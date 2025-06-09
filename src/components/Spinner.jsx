b6a78ec729f027c6ef438f47e9d2c3b1781cd693
santhosh-2504 <santhoshanantha58@gmail.com>
2025-06-09 06:35:15 +0530

    Spinner Component

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,9 +1,7 @@
 const Spinner = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div
-        className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-600"
-      />
+    <div className="fixed top-0 left-0 w-screen h-screen z-[9999] flex justify-center items-center">
+      <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
     </div>
   );
 };
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,90 @@
+// import { Provider } from "react-redux";
+// import { ToastContainer } from "react-toastify";
+// import { useEffect } from "react";
+// import store from "@/store/store";
+// import { getUser } from "@/store/slices/userSlice";
+// import Layout from "@/components/Layout/index";
+// import { ThemeProvider } from '@/context/ThemeContext';
+// import "@/styles/global.css";
+// import "react-toastify/dist/ReactToastify.css";
+
+// function AppContent({ Component, pageProps }) {
+//   useEffect(() => {
+//     store.dispatch(getUser());
+//   }, []);
+
+//   return (
+//     <ThemeProvider>
+//       <Layout>
+//         <Component {...pageProps} />
+//         <ToastContainer 
+//           position="top-right" 
+//           autoClose={3000}
+//           hideProgressBar={false}
+//           newestOnTop={false}
+//           closeOnClick
+//           rtl={false}
+//           pauseOnFocusLoss
+//           draggable
+//           pauseOnHover
+//           theme="light"
+//         />
+//       </Layout>
+//     </ThemeProvider>
+//   );
+// }
+
+// function MyApp(props) {
+//   return (
+//     <Provider store={store}>
+//       <AppContent {...props} />
+//     </Provider>
+//   );
+// }
+
+// export default MyApp;
+
+
 import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import store from "@/store/store";
 import { getUser } from "@/store/slices/userSlice";
 import Layout from "@/components/Layout/index";
-import { ThemeProvider } from '@/context/ThemeContext';
+import { ThemeProvider } from "@/context/ThemeContext";
+import Spinner from "@/components/Spinner";
 import "@/styles/global.css";
 import "react-toastify/dist/ReactToastify.css";
 
 function AppContent({ Component, pageProps }) {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
   useEffect(() => {
     store.dispatch(getUser());
   }, []);
 
+  useEffect(() => {
+    const handleStart = (url) => {
+      if (url !== router.asPath) setLoading(true);
+    };
+    const handleStop = () => setLoading(false);
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleStop);
+    router.events.on("routeChangeError", handleStop);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleStop);
+      router.events.off("routeChangeError", handleStop);
+    };
+  }, [router]);
+
   return (
     <ThemeProvider>
+      {loading && <Spinner />}
       <Layout>
         <Component {...pageProps} />
         <ToastContainer 
