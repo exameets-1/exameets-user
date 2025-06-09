f48d5b7c5dff6b6f8e7714f61b3d0c4fd943c209
santhosh-2504 <santhoshanantha58@gmail.com>
2025-06-09 08:14:23 +0530

    Spinner Changes

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,7 +1,7 @@
 const Spinner = () => {
   return (
     <div className="fixed top-0 left-0 w-screen h-screen z-[9999] flex justify-center items-center">
-      <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+      <div className="w-10 h-10 border-4 border-[#015990] border-t-transparent rounded-full animate-spin"></div>
     </div>
   );
 };
