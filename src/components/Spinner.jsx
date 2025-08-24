84770fe5286d96880cdb0a89738f0dc75416aef0
santhosh-2504 <santhoshanantha58@gmail.com>
2025-08-24 23:21:00 +0530

    Darkmode change for spinner

--- src/components/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-const Spinner = () => {
-  return (
-    <div className="fixed top-0 left-0 w-screen h-screen z-[9999] flex justify-center items-center">
-      <div className="w-10 h-10 border-4 border-[#015990] border-t-transparent rounded-full animate-spin"></div>
-    </div>
-  );
-};
-
-export default Spinner;
